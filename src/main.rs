c297d2515093ae4d7b5ab3d9646469b44ac4e90b
Denis K <konsistentsi@gmail.com>
2022-07-07 23:06:12 +1000

    WIP:

--- src/main.rs
+++ src/main.rs
@@ -22,22 +22,42 @@ fn home() -> Template {
 
 #[get("/answer/<id>")]
 fn answer(id: u64) -> Template {
+    //----------------------------------------------
     // TODO: get answer by id
-    println!("answer id:{}", id);
+    //----------------------------------------------
 
     Template::render(
         "answer",
         rocket_dyn_templates::context! {
-            answer: "42",
+            answer: id,
         },
     )
 }
 
 #[post("/question", data = "<form_data>")]
 async fn question(form_data: Form<FormData>) -> Redirect {
+    //----------------------------------------------
     // TODO: process the question
+    //----------------------------------------------
     println!("question: {}", form_data.question);
     sleep(Duration::from_secs(20)).await;
+
+    // // get reading
+    // let (hexagram, related) = wires::reading();
+    // // create new answer
+    // let new_answer = Answer::new(&data.email, &data.question, &hexagram, &related);
+    // let answer_id = new_answer.id.clone();
+    // // save answer
+    // Answer::insert(&connection, &new_answer)?;
+    // // send an email with the answer
+    // Answer::send(
+    //     &data.email,
+    //     &data.question,
+    //     &config.username,
+    //     &config.password,
+    //     &iching::full_answer(&connection, new_answer)?,
+    // );
+    //----------------------------------------------
 
     Redirect::to(format!("/answer/{}", 42))
 }
